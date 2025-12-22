3d6dc0f058d631eab7bf703c81ae0e852daeb585
GogoB <gorjangogo@hotmail.com>
2025-12-22 13:28:34 +0100

    End of Section 2

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task <ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = context.Users.ToList();
+
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = context.Users.Find(id); //only works when using primary key
+
+            if(member == null) return NotFound();
+            return member;
+        }
+    }
+}
